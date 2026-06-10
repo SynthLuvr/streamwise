3833daa1d58a4a6e96345c7bbb0c1ecec9e82218
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2026-06-10 19:35:29 +0200

    allow quit

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
 import { input } from "@inquirer/prompts";
 import { tools } from "./tools";
 
+const getInput = async () => {
+  try {
+    return await input({ message: "Ask anything" });
+  } catch {
+    return false;
+  }
+};
+
+const isExit = (message: string | false) => {
+  if (message === false) return true;
+  if (["quit", "exit", "q"].includes(message)) return true;
+  return false;
+};
+
 const main = async () => {
   console.debug("Loaded", tools.length, "tools");
-  const userInput = await input({ message: "Ask anything" });
-  console.log(userInput);
+
+  while (true) {
+    const userInput = await getInput();
+    if (isExit(userInput)) return;
+    console.log(userInput);
+  }
 };
 
 main();
